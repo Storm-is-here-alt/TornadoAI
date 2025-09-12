e3a6d7924a85afc2f344f611870632933e1189d9
Storm-is-here-alt <t3x.is.here@gmail.com>
2025-09-11 23:28:46 -0500

    Remove viewBinding usage

--- app/app/src/main/app/src/main/java/com/storm/tornadoai/MainActivity.kt
+++ app/app/src/main/app/src/main/java/com/storm/tornadoai/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.storm.tornadoai
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.storm.tornadoai.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        binding.textView.text = "TornadoAI is alive ✅"
+        findViewById<TextView>(R.id.textView).text = "TornadoAI is alive ✅"
     }
 }
