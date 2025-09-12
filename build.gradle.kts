28f60920dc0259c0488f2fa2786efb76e08ee44f
Storm-is-here-alt <t3x.is.here@gmail.com>
2025-09-11 22:06:33 -0500

    Fix root build.gradle.kts with plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.storm.tornadoai"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.storm.tornadoai"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "0.1"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-        debug { isDebuggable = true }
-    }
-
-    buildFeatures { viewBinding = true }
-    kotlinOptions { jvmTarget = "17" }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
