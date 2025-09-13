234852f8254ef86108fdf08574c5bb9b83ce135e
Storm-is-here-alt <t3x.is.here@gmail.com>
2025-09-13 12:51:48 -0500

    Update build.gradle.kts
    addressing duplicate calls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,12 @@
-plugins {
-    id("com.android.application")
-    kotlin("android")
-}
-
-android {
-    namespace = "com.storm.tornadoai"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.storm.tornadoai"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions { jvmTarget = "17" }
-}
-
-repositories {
-    google()
-    mavenCentral()
-}
-
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Needed for MainActivity.kt (OkHttp usage)
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    // If you use coroutines anywhere (safe to include)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 }
