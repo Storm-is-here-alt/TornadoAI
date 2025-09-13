ed14bf3a8c17d9580f554e0a286136eb4f106d8a
Storm-is-here-alt <t3x.is.here@gmail.com>
2025-09-13 08:17:55 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    kotlin("android") version "2.0.0" apply false
+    id("com.android.application") version "8.4.2" apply false
+    kotlin("android") version "1.9.24" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
