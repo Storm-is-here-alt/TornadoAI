637f18319dc5d33ace72d57805315e29ae1aac8f
Storm-is-here-alt <t3x.is.here@gmail.com>
2025-09-12 21:10:21 -0500

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,25 @@
+// settings.gradle.kts
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
 pluginManagement {
-    repositories { google(); mavenCentral(); gradlePluginPortal() }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        // JitPack only if/when you need it
+        maven(url = "https://jitpack.io")
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        google()
+        mavenCentral()
+        // keep JitPack here disabled unless needed
+        maven(url = "https://jitpack.io")
+    }
 }
+
 rootProject.name = "TornadoAI"
 include(":app")
