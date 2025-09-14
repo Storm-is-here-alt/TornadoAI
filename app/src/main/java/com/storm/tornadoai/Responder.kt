b9aba527b56a8c721e2dcb9d04cba499ca95622d
Storm-is-here-alt <t3x.is.here@gmail.com>
2025-09-13 19:01:28 -0500

    Update Responder.kt
    adding stability

--- app/src/main/java/com/storm/tornadoai/Responder.kt
+++ app/src/main/java/com/storm/tornadoai/Responder.kt
@@ -1,5 +1,2 @@
 package com.storm.tornadoai
-
-sealed class Responder {
-    enum class Intent { GREETING, HELP, DENY, OTHER }
-}
+sealed class Responder { enum class Intent { GREETING, HELP, DENY, OTHER } }
